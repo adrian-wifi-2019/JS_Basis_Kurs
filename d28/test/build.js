ce877e592c848c9fab98c6c88d5ed1a09fcabc10
Adrian Talotta <wifi@adriantalotta.com>
2019-04-24 18:09:53 +0200

    d28

---
+++ d28/test/build.js
@@ -0,0 +1,1 @@
+function addiere(n,a){var e=3;return n+a+e}console.log("Das Ergebnis von "+a+" + "+b+" ist "+(a+b));
---
+++ d28/test/test.js
@@ -0,0 +1,7 @@
+function addiere(foo1, foo2){
+  var blabla = 3;
+  return foo1 + foo2 + blabla;
+}
+
+console.log('Das Ergebnis von ' + a + ' + ' + b + ' ist ' + (a + b) );
+// console.log(`Das Ergebnis von ${a} + ${b} ist ${addiere(a, b)}`);
